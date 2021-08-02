b2913b3941707b5d09baaedcdb0c7082d093da83
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-08-03 00:20:12 +0700

    HowlerJS for Audio Library
    howler.js is an audio library for the modern web. It defaults to Web Audio API and falls back to HTML5 Audio.

--- src/app/pages/wedding.component.ts
+++ src/app/pages/wedding.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-wedding',
   templateUrl: './wedding.component.html',
-  styleUrls: ['./wedding.component.css']
+  styleUrls: ['./wedding.component.css'],
 })
 export class WeddingComponent implements OnInit {
+  
+  constructor() {
+    var sound = new Howl({
+      src: ['./assets/sound/sample.mp3'],
+      html5: true,
+    });
 
-  constructor() { }
-
-  ngOnInit(): void {
+    sound.play();
   }
 
+  ngOnInit(): void {}
 }
